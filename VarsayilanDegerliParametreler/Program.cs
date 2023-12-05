40b1385fc2f7f6d3e0679bf9da8d96301fb351d0
bolat <bolat@YELIZ>
2023-12-05 09:13:53 +0300

    Proje dosyası ekle.

---
+++ VarsayilanDegerliParametreler/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarsayilanDegerliParametreler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sinifim s = new Sinifim();
+            s.Selamla();
+            s.Selamla("arda ");
+        }
+    }
+    class Sinifim
+    {
+        public  void Selamla(string ad = "ali")
+        {
+            Console.WriteLine("merhaba " + ad);
+        }
+    }
+}
